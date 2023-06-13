ecccf20d82eb845c908c2ee8819eb979757b4bb9
alexdilion <alexdilion@protonmail.com>
2023-06-13 17:35:50 +0100

    Add ToDoItem factory

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ToDoItem from "./modules/ToDoItem";
+
+const x = ToDoItem("Interesting", 1);
+console.table(x.getProperties());
---
+++ src/modules/ToDoItem.js
@@ -0,0 +1,28 @@
+const ToDoItem = (itemTitle, itemId, itemDescription = "", itemPriority = 0, itemDue = "") => {
+    const properties = {
+        title: itemTitle,
+        id: itemId,
+        description: itemDescription,
+        priority: itemPriority,
+        due: itemDue,
+    };
+
+    const getProperty = (property) => properties[property];
+    const getProperties = () => properties;
+
+    const setProperty = (property, value) => {
+        if (!Object.prototype.hasOwnProperty.call(properties, property)) {
+            console.error(`${property} is not a property`);
+        }
+
+        properties[property] = value;
+    };
+
+    return {
+        getProperty,
+        getProperties,
+        setProperty,
+    };
+};
+
+export default ToDoItem;
