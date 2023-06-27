56906f8df615faaad428ade0269970a40ee7d181
alexdilion <alexdilion@protonmail.com>
2023-06-27 17:21:46 +0100

    Add task creation form

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import MicroModal from "micromodal";
+import flatpickr from "flatpickr";
+
 import ToDoItem from "./modules/ToDoItem";
 import ToDoList from "./modules/ToDoList";
 import ToDoProject from "./modules/ToDoProject";
@@ -6,14 +8,19 @@ import CreateItem from "./modules/dom/CreateItem";
 
 import "./style/style.css";
 import "./style/form.css";
+import "flatpickr/dist/flatpickr.css";
 
 MicroModal.init();
+flatpickr("#dt-due-date", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    minDate: "today",
+});
 
-const test = ToDoItem("TEsting", 1, "", "high", Date.now());
+const test = ToDoItem("Testingh lajsdlkjqw jlkeqwj ekaljsd jaslk daslkd ", 1, "", "high", Date.now());
 const test2 = ToDoItem("TEsting", 2, "");
 const test3 = ToDoItem("TEsting", 1, "", "medium", new Date(2023, 5, 24), true);
 
 CreateItem(test);
 CreateItem(test2);
 CreateItem(test3);
-
